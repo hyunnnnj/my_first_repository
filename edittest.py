337594f963aee1793f9875e07339383af5634180
hyunnnnj <124047728+hyunnnnj@users.noreply.github.com>
2023-09-04 18:22:52 +0900

    edittest.py

--- edittest.py
+++ edittest.py
@@ -12,14 +12,20 @@ print("python", a, b)
 
 number_school = 2394584903
 str_name = "python"
+print(number_school)
+print(str_name)
 
 bool_view = False
+print(bool_view)
 
 flt_pi = 3.14
+print(fit_pi)
 
 char_element = 'a'
+print(char_element)
 
 chars = "hello world"
+print(chars)
 
 print("number :", number_school,"name : ",str_name, "bool : ",bool_view, "float :",flt_pi,"charcter : ",char_element,"characters : ",chars)
 
